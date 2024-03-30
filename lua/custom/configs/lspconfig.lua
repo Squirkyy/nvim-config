75e83565546d634bb57cdb78f91d1280fdedc2fc
Darius Vollmer <dariusv@outlook.de>
2024-03-30 04:16:25 +0100

    Finally got c++ autoformatter running

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -8,6 +8,14 @@ lspconfig.clangd.setup {
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
 
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("FormatOnSave", {clear = true}),
+      buffer = bufnr,
+      callback = function ()
+        vim.lsp.buf.format({ bufnr = bufnr})
+      end,
+    })
+
     require("clangd_extensions.inlay_hints").setup_autocmd()
     require("clangd_extensions.inlay_hints").set_inlay_hints()
   end,
